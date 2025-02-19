dc3c1339cc37a185ed7dd02e50d93739df95da80
veshon <vashonmabima12@gmail.com>
2025-02-19 15:53:30 +0530

    UI Customer form

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,30 @@
-import {Link} from "react-router";
-import '../assets/style.css'
+import { Link } from "react-router-dom";
+import '../assets/style.css';
 
 export function Navigation() {
     return (
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        <div>
-                            <Link to='/' className="nav-link">Dashboard</Link>
-                            <Link to='/add' className="nav-link">Customer</Link>
-                            {/*going to add customer page*/}
-                            <Link to='/addItem' className="nav-link">Item</Link>
-                        </div>
-                        <br></br>
-                        <br></br>
-                        <Link to='/add'>Add Customer &nbsp;</Link>
-                        <Link to='/delete'>Delete Customer &nbsp;</Link>
-                        <Link to='/update'>Update Customer &nbsp;</Link>
-                    </ul>
-                </nav>
-
-                <nav>
-                    <ul>
-                        <Link to='/addItem'>Add Item &nbsp;</Link>
-                        <Link to='/deleteItem'>Delete Item &nbsp;</Link>
-                        <Link to='/updateItem'>Update Item &nbsp;</Link>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
+        <header>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li><Link to="/" className="nav-link">Dashboard</Link></li>
+                    <li className="dropdown">
+                        <span className="nav-link">Customer ▼</span>
+                        <ul className="dropdown-menu">
+                            <li><Link to="/add" className="nav-link">Add Customer</Link></li>
+                            <li><Link to="/delete" className="nav-link">Delete Customer</Link></li>
+                            <li><Link to="/update" className="nav-link">Update Customer</Link></li>
+                        </ul>
+                    </li>
+                    <li className="dropdown">
+                        <span className="nav-link">Items ▼</span>
+                        <ul className="dropdown-menu">
+                            <li><Link to="/addItem" className="nav-link">Add Item</Link></li>
+                            <li><Link to="/deleteItem" className="nav-link">Delete Item</Link></li>
+                            <li><Link to="/updateItem" className="nav-link">Update Item</Link></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
